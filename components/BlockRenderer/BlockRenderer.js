5ec7f8f59778f819c78175a8354106c6eda30381
BrettMadrid <brettmadrid9@gmail.com>
2024-10-29 12:26:53 -0700

    Cleaned Up Code

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -2,7 +2,7 @@ export const BlockRenderer = ({ blocks }) => {
   return blocks.map((block) => {
     switch (block.name) {
       case "core/cover": {
-        return <div key={block.id}>Core Cover</div>;
+        return <div key={block.id}>Puffy</div>;
       }
       default:
         return null;
--- utils/cleanAndTransformBlocks.js
+++ utils/cleanAndTransformBlocks.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from "uuid";
 
-export const cleanAndTransformBlocks = (blocksJSON) => {
-  const blocks = JSON.parse(JSON.stringify(blocksJSON));
+export const cleanAndTransformBlocks = (blocksObj) => {
+  const blocks = JSON.parse(JSON.stringify(blocksObj));
 
   const assignIds = (b) => {
     b.forEach((block) => {
